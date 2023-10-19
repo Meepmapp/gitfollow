31a8eaf1e4f4b1a3c86e6c8633a2a7bfb2b8e0f5
Khoa Van Hoang <khoa.hoang@student.manchester.ac.uk>
2023-10-19 20:35:38 +0700

    abc

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(sumNumber(2, 3));
         System.out.println("Hello world!");
         System.out.println("Nope" );
+
+    }
+    private static int sumNumber (int a, int b){
+        return a+b;
     }
 }
